120cdbc7286cae4402712dd65c825a067f82ce9a
Carl Closs <ccloss1@cs.binghamton.edu>
2018-03-07 22:28:35 -0500

    Optimal policy complete and probably bug-free

--- policies.hpp
+++ policies.hpp
@@ -5,8 +5,10 @@
 #include <vector>
 
 // PRP function pointer type
-typedef int (*PageReplacementPolicy)(const std::vector<int>&);
+typedef int (*PageReplacementPolicy)(const std::vector<int>&, int); 
+//Added memsize param to the function type, because this varies between runs as well.
 
-int PRP_FIFO(const std::vector<int>& workload);
+int PRP_FIFO(const std::vector<int>& workload, int memsize);
+int PRP_OPT(const std::vector<int>& workload, int memsize);
 
 #endif /* end of include guard: POLICIES_H_ */
