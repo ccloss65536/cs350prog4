3bc8292a8e283f385a3975e5f3bee80b9c9b6f7a
Carl Closs <ccloss1@cs.binghamton.edu>
2018-03-14 15:16:15 -0400

    MESSAGE

--- prog4pagepolicy.cpp
+++ prog4pagepolicy.cpp
@@ -4,8 +4,13 @@
 #include "workloads.h"
 #include "policies.h"
 
+#define NUM_WORKLOADS 3
+#define NUM_POLICIES 5
+#define NUM_ACCESSES 10000
+
 int main(int argc, char** argv){
 	ofstream("data.csv");
-	//...
+	Workload workloads[NUM_WORKLOADS] = {workload_nonlocal, workload_80_20, workload_looping};
+	PageReplacementPolicy policies[NUM_POLICIES] = {PRP_FIFO, PRP_OPT, PRP_RAND}
 }
 
